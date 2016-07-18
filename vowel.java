c62f9d4285d5496b1132748eab349a9eed3385de
vignesh7balakrish <vigneshbala38@gmail.com>
2016-07-18 14:00:59 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class vowel
+{
+  public static void main(String args[])
+  {
+    char ch;
+    BufferedReader obj=new BufferedReader(new InputStreamReader(System.in));
+    ch=(char)obj.read();
+    if(ch=='a' && ch=='e' && ch=='i' && ch=='o' && ch='u')
+    {
+      System.out.println("vowel");
+    }
+    else
+    {
+    System.out.println("constants");
+    }
+    
+    }
+    }
+    
+    
