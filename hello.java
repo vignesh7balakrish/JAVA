e2ad9d84c16cddc8bddc75fd27be843f8a4a822f
vignesh7balakrish <vigneshbala38@gmail.com>
2016-07-18 14:20:54 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class hello
+{
+  public static void main(String args[])
+  {
+    String Str="hello";
+    for(int i=0;i<5;i++)
+    {
+      System.out.println(Str);
+    }
+  }
+}
