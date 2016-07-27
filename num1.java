16b9da617d95d7cb0b724f5d14feed931d5b5a40
vignesh7balakrish <vigneshbala38@gmail.com>
2016-07-27 14:18:03 +0530

    Create num1.java

---
+++ num1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class num1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str;
+		Scanner scan=new Scanner (System.in);
+		str=scan.nextLine();
+		int num=Integer.parseInt(str);
+		System.out.println(num);
+	}
+
+}
