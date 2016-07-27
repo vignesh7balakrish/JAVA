ef3855c774e87fe7006b2817b396026fb1a36723
vignesh7balakrish <vigneshbala38@gmail.com>
2016-07-27 14:31:53 +0530

    Create min.java

---
+++ min.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class min {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num,num1;
+		Scanner scan=new Scanner(System.in);
+		num=scan.nextInt();
+		num1=scan.nextInt();
+		if(num1<num)
+		{
+			System.out.println(num1);
+		}
+		else
+			System.out.println(num);
+	}
+
+}
