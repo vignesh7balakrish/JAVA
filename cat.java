f87cccf9cb893fa752712f3ce77a3c1d582c275b
vignesh7balakrish <vigneshbala38@gmail.com>
2016-08-25 13:47:41 +0530

    Create cat.java

---
+++ cat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class cat
+{
+  public static void main(String args[])
+  {
+    String str,str1;
+    Scanner scan=new Scanner(System.in);
+    str=scan.nextLine();
+    str1=scan.nextLine();
+    System.out.print(str+str1);
+  }
+}
