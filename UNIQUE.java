2de6998a4fab46ba8e055fd870115763325c9d9a
vignesh7balakrish <vigneshbala38@gmail.com>
2016-08-25 21:52:47 +0530

    Create UNIQUE.java

---
+++ UNIQUE.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class unique
+{
+  public static void main(String args[])
+  {
+    String str;
+    Scanner sc=new Scanner(System.in);
+    str=sc.nextLine();
+    char a[]=str.toCharArray();
+    int a1[]=new int[a.length];
+    for(int i=0;i<a.length;i++)
+    {
+      a1[i]=Character.get
