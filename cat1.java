94ef1a97cc8f694002b50f0a155eb0a1ec78abc5
vignesh7balakrish <vigneshbala38@gmail.com>
2016-08-25 13:51:33 +0530

    Create cat1.java

---
+++ cat1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class cat1
+{
+  public static void main(String args[])
+  {
+    String str;
+    Scanner sc=new Scanner(System.in);
+    str=nextLine();
+    String str1[]=str.split(" ");
+    System.out.println(str1.length+"is word");
+  }
+}
+    
